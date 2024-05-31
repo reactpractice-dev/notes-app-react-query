c76ef198e905093cf55b72350155de771ec44f35
Corina Udrescu <corinutz@gmail.com>
2024-05-31 11:31:55 +0200

    simpler pinning

--- src/api/pin-note.js
+++ src/api/pin-note.js
@@ -8,33 +8,9 @@ export const usePinNote = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: patchNote,
-    // When mutate is called:
-    onMutate: async (updatedNote) => {
-      // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-      await queryClient.cancelQueries(["notes"]);
-
-      // Snapshot the previous value
-      const previousNotes = queryClient.getQueryData(["notes"]);
-
-      // Optimistically update to the new value
-      queryClient.setQueryData(["notes"], (notes) =>
-        notes.map((n) =>
-          n.id === updatedNote.id
-            ? { ...n, is_pinned: updatedNote.is_pinned }
-            : n
-        )
-      );
-
-      // Return a context object with the snapshotted value
-      return { previousNotes };
-    },
-    // If the mutation fails, use the context returned from onMutate to roll back
-    onError: (err, updatedNote, context) => {
-      queryClient.setQueryData(["notes"], context.previousNotes);
-    },
-    // Always refetch after error or success:
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    onSuccess: () => {
+      // Invalidate and refetch
+      return queryClient.invalidateQueries({ queryKey: ["notes"] });
     },
   });
 };
